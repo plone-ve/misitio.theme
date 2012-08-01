299175e6e565bafa0f9c08a68517301cddfd871b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-08-01 17:05:15 -0430

    firist commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This package is a Plone 4 theme product for misitio website.
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='misitio.theme',
+      version=version,
+      description="An installable theme for Plone 4 for misitio website",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='web zope plone4 theme misitio website',
+      author='Leonardo J. Caballero G.',
+      author_email='leonardocaballero@gmail.com',
+      url='https://github.com/plone-ve/misitio.theme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['misitio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
